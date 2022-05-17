591fdd84912f0698b92b2c917b373b4c7c45eee3
Whitekashin <patrick.reitbauer@web.de>
2022-05-17 14:40:25 +0200

    Implementation of the logic for liking and deleting Posts, also Cleanup.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -29,16 +29,18 @@ const Form = ({ currentId, setCurrentId }) => {
         } else {
             dispatch(createPost(postData));
         }
+        clear();
     }
 
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({creator: '', title: '', message: '', tags: '', selectedFile: ''});
     }
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-            <Typography variant="h6">Creating a Memory</Typography>
+            <Typography variant="h6">{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
             <TextField 
             name="creator" 
             variant="outlined" 
@@ -69,7 +71,7 @@ const Form = ({ currentId, setCurrentId }) => {
             label="Tags" 
             fullWidth
             value={postData.tags}
-            onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
+            onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}/>
             <div className={classes.fileInput}>
                 <FileBase 
                     type="file"
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -5,10 +5,14 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 
+import { useDispatch } from 'react-redux';
+import { deletePost, likePost } from '../../../actions/posts';
+
 import useStyles from './styles';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
 
     return (
         <Card className={classes.card}>
@@ -31,16 +35,19 @@ const Post = ({ post, setCurrentId }) => {
                 {post.tags.map((tag) => `#${tag} `)}
             </Typography>
             </div>
+            <Typography className={classes.title} variant="h5">
+                {post.title}
+            </Typography>
         <CardContent>
-            <Typography className={classes.title} variant="h5" gutterBottom>
+            <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
                 {post.message}
             </Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}}>
-                    <ThumbUpAltIcon fontSize="small"/> Like {post.likeCount}
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
+                    <ThumbUpAltIcon fontSize="small"/> &nbsp; Like &nbsp; {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small"/> Delete 
                 </Button>
             </CardActions>
